f90d2549235af7000b12cd36f04dc997742bfc2b
vsesvit <sfrol91@gmail.com>
2023-08-19 11:30:39 +0200

    counts for matrix

--- kata_cubes/Program.cs
+++ kata_cubes/Program.cs
@@ -15,6 +15,50 @@
     return result;
 };
 
+static int[] KataCubesLeft(int[] initialDat, char direction)
+{
+    int width = initialDat.Length;
+    int height = initialDat.Max();
+    int[,] matrix = new int[width, height];
+    //int[,] matrixLeft = new int[width, height];
+    //int[] resultForLeft = new int[height];
+
+    int[] count = new int[height];
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = height - initialDat[i]; j < initialDat.Max(); j++)
+        {
+            count[j]+=1;
+            matrix[i, j] = 1;
+        }
+    }
+    /*for (int i = 0; i < count.Length; i++)
+    {
+        for (int k = 0; k < count.Length; k++)
+        {
+         
+            matrixLeft[i, k] = 1;
+           
+        }
+    }
+    Console.WriteLine(matrixLeft);
+    DrawMatrix(matrixLeft);*/
+    DrawMatrix(matrix);
+    WriteArray(count);
+
+
+    return count; 
+}
+
+static void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+    Console.WriteLine();
+}
 
 static void DrawMatrix(int[,] check)
 
@@ -33,4 +77,7 @@ static void DrawMatrix(int[,] check)
 }
 
 int[] example = new int[] { 3, 2, 3, 1, 2 };
-KataCubes(example, 'R');
+//KataCubes(example, 'R');
+int[] horizontal = KataCubesLeft(example, 'L');
+//Left(horizontal);
+
