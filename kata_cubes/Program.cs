096a7fced748f81742470f925438690a106b8810
vsesvit <sfrol91@gmail.com>
2023-08-22 09:40:31 +0200

    small changes

--- kata_cubes/Program.cs
+++ kata_cubes/Program.cs
@@ -1,56 +1,38 @@
-﻿/*static int[] KataCubes(int[] initialDat, char direction)
+﻿static int[] KataCubes(int[] initialDat, char direction)
 {
     int width = initialDat.Length;
     int height = initialDat.Max();
     int[,] matrix = new int[width, height];
-    int[] result = new int[width];
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = height - initialDat[i]; j < height; j++)
-        {
-            matrix[i, j] = 1;
-        }
-    }
-    DrawMatrix(matrix);
-    return result;
-};*/
-
-static int[] KataCubes(int[] initialDat, char direction)
-{
-    int width = initialDat.Length;
-    int height = initialDat.Max();
-    int[,] matrix = new int[width, height];
-    // int[,] matrixLeft = new int[width, height];
-    //int[,] matrixRight = new int[width, height];
-    //int[] resultForLeft = new int[height];
-
+    int[,] matrixLeft = new int[width, height];
+    int[,] matrixRight = new int[width, height];
     int[] count = new int[height];
     for (int i = 0; i < width; i++)
     {
         for (int j = height - initialDat[i]; j < height; j++)
         {
-            count[j] += 1;
+            
             matrix[i, j] = 1;
+            count[j] += 1;
             if (direction == 'L')
             {
-                for (int k = 0; k < count[j]; k++)
+                for (int k = 0; k < count[j] ; k++)
                 {
-                    matrix[k, j] = 1;
+                    matrixLeft[k, j] = 1;
                 }
             }
             if (direction == 'R')
             {
-                for (int k = width - count[j]; k < width; k++)
+                for (int k = width - count[j]; k < width ; k++)
                 {
-                    matrix[k, j] = 1;
+                    matrixRight[k, j] = 1;
                 }
             }
         }
     }
     DrawMatrix(matrix);
     WriteArray(count);
-    // DrawMatrix(matrixLeft);
-    // DrawMatrix(matrixRight);
+    DrawMatrix(matrixLeft);
+    DrawMatrix(matrixRight);
     return count;
 }
 
@@ -81,7 +63,25 @@ static void DrawMatrix(int[,] check)
 
 int[] example = new int[] { 3, 2, 3, 1, 2 };
 
-//KataCubes(example, 'R');
-int[] horizontal = KataCubes(example, 'L');
+
+int[] right = KataCubes(example, 'R');
+int[] left = KataCubes(example, 'L');
 //Left(horizontal);
+
+/*static int[] KataCubes(int[] initialDat, char direction)
+{
+    int width = initialDat.Length;
+    int height = initialDat.Max();
+    int[,] matrix = new int[width, height];
+    int[] result = new int[width];
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = height - initialDat[i]; j < height; j++)
+        {
+            matrix[i, j] = 1;
+        }
+    }
+    DrawMatrix(matrix);
+    return result;
+};*/
 
